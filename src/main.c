56788369bd7f66b31e54d3b5d83ba1a3c92a81d6
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-06-10 22:01:08 -0600

    reading ffmpeg's code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+// TODO: build ffmpeg -- DONE
+// TODO: read decode_video.c -- DONE
+// TODO: read decode_filter_video.c -- DONE
+// TODO: read vaapi_encode.c
+// TODO: take notes on how decode_video.c works -- DONE
+// TODO: take notes on how decode_filter_video.c works -- DONE
+// TODO: take notes on how vaapi_encode.c works
+// TODO: include ffmpeg headers
+// TODO: load video file using ffmpeg
+// TODO: ff_decode_receive_frame
+// TODO: trace down ffmpeg function using debugger
+
 int main() {
     printf("Hello world!\n");
 }
