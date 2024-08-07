a230fdec43cec5c4677fed96e618805166e5a55e
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-08-06 20:16:22 -0600

    working demo

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,76 @@
-use ffmpeg_next as ffmpeg;
+use ffmpeg_next::codec::context::Context;
+use ffmpeg_next::decoder::audio::Audio;
+use ffmpeg_next::decoder::video::Video;
+use ffmpeg_next::format::context::Input;
+use ffmpeg_next::frame;
+use ffmpeg_next::media::Type;
+use ffmpeg_next::Packet;
 
-// we could store audio and video decoder in the same struct
-// audio and video could be seperate -- then we could put scaling in their respective structs
-
-struct Decoder {
-    index: usize,
-    decoder: ffmpeg::decoder::Decoder,
+struct VideoDecoder {
+    stream_index: usize,
+    decoder: Video,
 }
 
-impl Decoder {
-    pub fn new(is_video: bool, context: ffmpeg::format::context::Input) -> Self {
-        let media = context.streams().best(ffmpeg::media::Type::Video).unwrap();
-        let index = media.index();
-        let codec_context =
-            ffmpeg::codec::context::Context::from_parameters(media.parameters()).unwrap();
-        let decoder = codec_context.decoder();
-        Self { index, decoder }
+impl VideoDecoder {
+    pub fn new(context: &Input) -> Self {
+        let media = context.streams().best(Type::Video).unwrap();
+        let codec_context = Context::from_parameters(media.parameters()).unwrap();
+        let decoder = codec_context.decoder().video().unwrap();
+        Self {
+            stream_index: media.index(),
+            decoder,
+        }
     }
 
-    pub fn decode(&mut self, packet: &ffmpeg::frame::Packet) {
+    fn decode(&mut self, packet: &Packet) {
         self.decoder.send_packet(packet).unwrap();
 
-        let mut frame = ffmpeg::frame::Video::empty();
+        let mut frame = frame::Video::empty();
         while self.decoder.receive_frame(&mut frame).is_ok() {
             println!("Video frame: {}x{}", frame.width(), frame.height());
         }
     }
 }
 
+struct AudioDecoder {
+    stream_index: usize,
+    decoder: Audio,
+}
+
+impl AudioDecoder {
+    pub fn new(context: &Input) -> Self {
+        let media = context.streams().best(Type::Audio).unwrap();
+        let codec_context = Context::from_parameters(media.parameters()).unwrap();
+        let decoder = codec_context.decoder().audio().unwrap();
+        Self {
+            stream_index: media.index(),
+            decoder,
+        }
+    }
+
+    fn decode(&mut self, packet: &Packet) {
+        self.decoder.send_packet(packet).unwrap();
+
+        let mut frame = frame::Audio::empty();
+        while self.decoder.receive_frame(&mut frame).is_ok() {
+            println!("Audio frame!");
+        }
+    }
+}
+
 fn main() {
-    ffmpeg::init().unwrap();
+    ffmpeg_next::init().unwrap();
 
     let path = "/home/aabiji/Videos/sync-test.mp4";
-    let mut format_context = ffmpeg::format::input(path).unwrap();
-    let mut decoder = Decoder::new(true);
+    let mut format_context = ffmpeg_next::format::input(path).unwrap();
+    let mut video_decoder = VideoDecoder::new(&format_context);
+    let mut audio_decoder = AudioDecoder::new(&format_context);
 
     for (stream, packet) in format_context.packets() {
-        if stream.index() == decoder.index {
-            decoder.decode(&packet);
+        if stream.index() == video_decoder.stream_index {
+            video_decoder.decode(&packet);
+        } else if stream.index() == audio_decoder.stream_index {
+            audio_decoder.decode(&packet);
         }
     }
 
